f780e0088362fe6ad9fd4315b49aac135af283f6
Tetteh399 <71871360+Tetteh399@users.noreply.github.com>
2023-07-11 15:34:03 +0000

    my first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from './Components/Header/Header';
+import { MainRoutes } from './Routes/MainRoutes';
+
+function App() {
+	return (
+		<>
+			<Header />
+			<MainRoutes />
+		</>
+	);
+}
+
+export default App;
